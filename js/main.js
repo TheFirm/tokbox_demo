fc0237eab579ac9759eb9d1d451c3dd43f4a01c8
sheppard <vivchar.danylo@gmail.com>
2014-10-25 00:06:20 +0300

    chat demo

--- js/main.js
+++ js/main.js
@@ -1,60 +1,60 @@
 
 function initChat (){
-    console.log('TBAPP.apiKey', TBAPP.apiKey);
-    console.log('TBAPP.sessionId', TBAPP.apiKeysessionId);
 // Initialize session, set up event sessionId, and connect
-    var session = OT.initSession(TBAPP.apiKey, TBAPP.apiKeysessionId);
+    TBAPP.session = OT.initSession(TBAPP.key.key, TBAPP.key.sessionId);
 
-    session.on("streamCreated", function(event) {
+    TBAPP.session.on("streamCreated", function(event) {
         toastr.info("Stream created");
         console.log("streamCreated");
         var subscriberProperties = {insertMode: "append"};
 
-        var subscriber = session.subscribe(event.stream, 'connected', subscriberProperties, function (error) {
-            if (error) {
-                toastr.info("Sorry, something went wrong ... ( " + error + " )");
-                console.log(error);
-            } else {
+        var subscriber = TBAPP.session.subscribe(event.stream, 'connected', subscriberProperties, function (error) {
+            TBAPP.helpers.logIfError(error);
+            if (!error) {
                 toastr.info("Someone want to chat with you ...");
-                console.log("Subscriber added.");
             }
         });
+        TBAPP.subscribers.push(subscriber);
     });
 
-    session.on("streamDestroyed", function (event) {
+    TBAPP.session.on("streamDestroyed", function (event) {
         toastr.info("Someone left chat ...");
         console.log("Stream stopped streaming. Reason: " + event.reason)
     });
 
-    var publisher;
-    var token = TBAPP.token;
-
-    session.connect(token, function(error) {
-        publisher = OT.initPublisher('publisher');
-        session.publish(publisher);
-
-        //publisher.on("streamDestroyed", function (event) {
-        //
-        //    console.log("Stream stopped streaming. Reason: " + event.reason)
-        //});
+    TBAPP.session.connect(TBAPP.key.token, function(error) {
+        TBAPP.helpers.logIfError(error);
+        TBAPP.publisher = OT.initPublisher('publisher', {}, null);
+        TBAPP.session.publish(TBAPP.publisher, {}, null);
     });
 
-    session.on("signal", function(event) {
+    TBAPP.session.on("signal", function(event) {
+        if(event.data && event.data.fromUsername){
+            var $discussion = $('#chatWrapper .discussion');
+            var appendElem = '';
+            if(event.data.fromUsername == TBAPP.user.username){
+                appendElem = '<li class="self">\n    <div class="messages"><span class="user">' + 'me' + '</span>\n        <p>' + event.data.text + '</p>\n    </div>\n</li>';
+            } else {
+                appendElem = '<li class="other">\n    <div class="messages"><span class="user">' + event.data.fromUsername + '</span>\n        <p>' + event.data.text + '</p>\n    </div>\n</li>';
+            }
+            $discussion.append($(appendElem));
+        }
         console.log("Signal sent from connection " , event);
         // Process the event.data property, if there is any data.
     });
 
-    window.testM = function () {
-        session.signal(
+    TBAPP.sendMessage = function (text) {
+        TBAPP.session.signal(
             {
-                data:"hello",
+                data: {
+                    fromUsername: TBAPP.user.username,
+                    text: text
+                },
                 type:"textMessage"
             },
             function(error) {
                 if (error) {
-                    console.log("signal error ("
-                    + error.code
-                    + "): " + error.reason);
+                    console.log("signal error" + error);
                 } else {
                     console.log("signal sent.");
                 }
@@ -63,5 +63,14 @@ function initChat (){
     }
 }
 
+function requestUserInfo(){
+    TBAPP.user.username = prompt('Your name: ', new Date().getTime()+"");
+}
+
+requestUserInfo();
 initChat();
 
+$('#sendText').click(function (evt) {
+    var msg = $('#chatInput').val();
+    TBAPP.sendMessage(msg);
+});
