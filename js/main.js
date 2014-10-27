02c4178a38ec757a20568da07b0a31b4f901c301
sarates <stetsyuk.t@gmail.com>
2014-10-27 15:08:28 +0200

    fix

--- js/main.js
+++ js/main.js
@@ -78,7 +78,11 @@ function requestUserInfo(){
     TBAPP.user.username = prompt('Your name: ', new Date().getTime()+"");
 }
 
-
+/*
+$(".enter_name_submit").click(function(){
+    TBAPP.user.username = $("#name_input").val();
+});
+*/
 
 requestUserInfo();
 initChat();
