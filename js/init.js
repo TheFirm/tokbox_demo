f42baebe860195fb8dbf5914c0eb789d6490e9ec
sheppard <vivchar.danylo@gmail.com>
2015-10-30 16:27:19 +0200

    updated tokens

--- js/init.js
+++ js/init.js
@@ -5,9 +5,9 @@ var TBAPP = TBAPP || {};
 // Think of a session as a room, and a key.token as the key to get in to the room
 // Sessions and tokens are generated on your server and passed down to the client
 TBAPP.key = {
-    key: "45041112",
-    sessionId: "1_MX40NTA0MTExMn5-MTQxNDA4NzUwOTI2MX5zdzYvVTBlbEJyWklSSW9qc3ZwNVljQld-fg",
-    token: "T1==cGFydG5lcl9pZD00NTA0MTExMiZzaWc9ZjE5ZTIzNjlmYzJlNTE2NTQ4ZTU3M2IzYWY0ZGQ4NjQwNjFmOGYwMzpyb2xlPXB1Ymxpc2hlciZzZXNzaW9uX2lkPTFfTVg0ME5UQTBNVEV4TW41LU1UUXhOREE0TnpVd09USTJNWDV6ZHpZdlZUQmxiRUp5V2tsU1NXOXFjM1p3TlZsalFsZC1mZyZjcmVhdGVfdGltZT0xNDE0MDg3NTEyJm5vbmNlPTAuMzcxODI5OTEzNTA4NzAwODcmZXhwaXJlX3RpbWU9MTQxNjY3OTUwMQ=="
+    key: "45393382",
+    sessionId: "1_MX40NTM5MzM4Mn5-MTQ0NjIxNDkwMzc1OX5ZSURxUmI0WGtiamcyc0x6aEZrTVpHa2t-UH4",
+    token: "T1==cGFydG5lcl9pZD00NTM5MzM4MiZzaWc9NGE1NTFkMWViYjllMmY0NjQ4NTc1ODZlYTI5ZTczNDU0MTA0OGRhMzpyb2xlPXB1Ymxpc2hlciZzZXNzaW9uX2lkPTFfTVg0ME5UTTVNek00TW41LU1UUTBOakl4TkRrd016YzFPWDVaU1VSeFVtSTBXR3RpYW1jeWMweDZhRVpyVFZwSGEydC1VSDQmY3JlYXRlX3RpbWU9MTQ0NjIxNDkyMyZub25jZT0wLjA3NTk2MDkyMDAyNDY0MzE2JmV4cGlyZV90aW1lPTE0NDYyMTg0OTcmY29ubmVjdGlvbl9kYXRhPQ=="
 };
 
 TBAPP.subscribers = [];
